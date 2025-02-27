fe72ec4a48ed6066f5636f3d1328574966294b3c
Sebastian Rodriguez <ply.srh@gmail.com>
2025-02-27 19:43:09 -0300

    chore: move the catalog page to its own directory

---
+++ src/app/catalog/page.tsx
@@ -0,0 +1,20 @@
+import { Suspense } from "react";
+import Catalog from "@/components/catalog/Catalog";
+import CatalogSkeleton from "@/components/catalog/Catalog";
+
+interface HomeProps {
+  searchParams: Record<string, string>;
+}
+
+export default async function CatalogPage({ searchParams }: HomeProps) {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between'>
+      <div className="container w-full px-6 py-12">
+        <h1 className="font-bold mb-10 text-4xl text-neutral-800">Top Sellers</h1>
+        <Suspense fallback={<CatalogSkeleton searchParams={searchParams} />}>
+          <Catalog searchParams={searchParams} />
+        </Suspense>
+      </div>
+    </main>
+  )
+}
