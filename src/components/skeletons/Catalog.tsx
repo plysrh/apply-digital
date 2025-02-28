1dbfd10caeb69b86660e00d004c1da0f74bf02e2
Sebastian Rodriguez <ply.srh@gmail.com>
2025-02-28 17:06:58 -0300

    chore: improving the catalog skeleton

--- src/components/skeletons/Catalog.tsx
+++ src/components/skeletons/Catalog.tsx
@@ -1,36 +1,43 @@
 export default function Catalog() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-12">
-      <div className="rounded-xl animate-pulse">
-        <div className="w-full h-96 bg-neutral-200 rounded-xl" />
+    <>
+      <div className="flex text-2xl justify-end text-neutral-500 mb-10">
+        <div className="rounded-xl animate-pulse">
+          <div className="w-28 h-7 bg-neutral-200 rounded-xl" />
+        </div>
       </div>
-      <div className="rounded-xl animate-pulse">
-        <div className="w-full h-96 bg-neutral-200 rounded-xl" />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-12">
+        <div className="rounded-xl animate-pulse">
+          <div className="w-full h-96 bg-neutral-200 rounded-xl" />
+        </div>
+        <div className="rounded-xl animate-pulse">
+          <div className="w-full h-96 bg-neutral-200 rounded-xl" />
+        </div>
+        <div className="rounded-xl animate-pulse">
+          <div className="w-full h-96 bg-neutral-200 rounded-xl" />
+        </div>
+        <div className="rounded-xl animate-pulse">
+          <div className="w-full h-96 bg-neutral-200 rounded-xl" />
+        </div>
+        <div className="rounded-xl animate-pulse">
+          <div className="w-full h-96 bg-neutral-200 rounded-xl" />
+        </div>
+        <div className="rounded-xl animate-pulse">
+          <div className="w-full h-96 bg-neutral-200 rounded-xl" />
+        </div>
+        <div className="rounded-xl animate-pulse">
+          <div className="w-full h-96 bg-neutral-200 rounded-xl" />
+        </div>
+        <div className="rounded-xl animate-pulse">
+          <div className="w-full h-96 bg-neutral-200 rounded-xl" />
+        </div>
+        <div className="rounded-xl animate-pulse">
+          <div className="w-full h-96 bg-neutral-200 rounded-xl" />
+        </div>
+        <div className="rounded-xl animate-pulse">
+          <div className="w-full h-96 bg-neutral-200 rounded-xl" />
+        </div>
       </div>
-      <div className="rounded-xl animate-pulse">
-        <div className="w-full h-96 bg-neutral-200 rounded-xl" />
-      </div>
-      <div className="rounded-xl animate-pulse">
-        <div className="w-full h-96 bg-neutral-200 rounded-xl" />
-      </div>
-      <div className="rounded-xl animate-pulse">
-        <div className="w-full h-96 bg-neutral-200 rounded-xl" />
-      </div>
-      <div className="rounded-xl animate-pulse">
-        <div className="w-full h-96 bg-neutral-200 rounded-xl" />
-      </div>
-      <div className="rounded-xl animate-pulse">
-        <div className="w-full h-96 bg-neutral-200 rounded-xl" />
-      </div>
-      <div className="rounded-xl animate-pulse">
-        <div className="w-full h-96 bg-neutral-200 rounded-xl" />
-      </div>
-      <div className="rounded-xl animate-pulse">
-        <div className="w-full h-96 bg-neutral-200 rounded-xl" />
-      </div>
-      <div className="rounded-xl animate-pulse">
-        <div className="w-full h-96 bg-neutral-200 rounded-xl" />
-      </div>
-    </div>
+    </>
   );
 }
