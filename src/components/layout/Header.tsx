3c7240cb4f561e7c61362b2e8a1d62436e2f5e78
Sebastian Rodriguez <ply.srh@gmail.com>
2025-02-28 21:09:23 -0300

    chore: Adding the shopping link (logo or something?)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,9 +2,13 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="bg-neutral-200 w-full flex justify-center p-5">
-      <div className="container flex">
-        <div className="m-0" />
+    <header className="bg-neutral-200 w-full flex justify-center">
+      <div className="container flex p-5">
+        <div className="m-0">
+          <Link href="/" className="text-2xl text-neutral-500">
+            GamerShop
+          </Link>
+        </div>
         <div className="m-0 ml-auto">
           <Link className="relative" href="/cart">
             <img src="/shop.svg" className="cursor-pointer" />
